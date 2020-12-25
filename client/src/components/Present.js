44014854a4c91aa72ef1fe7fb50c360a4cdd25cc
Kathy Kang <kck3due@virginia.edu>
2020-12-25 15:35:29 -0500

    div changes

--- client/src/components/Present.js
+++ client/src/components/Present.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Coal from '../images/coal.png';
 import Socks from '../images/regifted_socks.png';
@@ -10,56 +10,66 @@ import Star from '../images/star.png';
 import './CSS/Present.css';
 
 class Present extends React.Component {
-    state = { id: 'noStar', image: '', title: '', message: '', href: '' };
+    state = { id: 'noStar', image: '', title: '', message: '', href: '', play: false };
+
+    audio = new Audio("https://www.youtube.com/watch?v=fJQqOzkcHjg&ab_channel=ChristmasSongsandCarols-LovetoSing");
 
     //when the page loads, determine which present the user gets
     componentDidMount = () => {
+        this.audio.addEventListener('ended', () => this.setState({ play: false }));
+
         const id = parseFloat(this.props.match.params.id);
         // top tier
-        if(id < 3){
+        if (id < 3) {
             this.setState({
-                id: "star", 
-                image: Star, 
-                title: "The Star", 
+                id: "star",
+                image: Star,
+                title: "The Star",
                 message: "Congratulations! There is only a 2% chance of getting this, you will be blessed with much luck and good fortune next year.",
                 href: ""
             });
         }
         // good tier
-        else if(id >= 3 && id < 10){
+        else if (id >= 3 && id < 10) {
             this.setState({
-                image: Charity, 
-                title: "Charity", 
+                image: Charity,
+                title: "Charity",
                 message: "You only have an 8% chance of getting this, so why not take this chance to help another soul? To donate, click on the image and it will redirect you to a page of charities.",
                 href: "https://www.gofundme.com/c/holiday?utm_source=google&utm_medium=cpc&utm_campaign=JoG_Search&utm_content=charity%20donation%20websites&utm_term=charity%20donation%20websites_e_c_&gclid=Cj0KCQiAuJb_BRDJARIsAKkycUnNMRpmU5rzPljCd_GpxG2x4zBnRdDM9AwrgHXuXT7_RafkC_ewBmAaAuHHEALw_wcB"
             });
         }
         // ok tier
-        else if(id >= 10 && id < 30){
+        else if (id >= 10 && id < 30) {
             this.setState({
-                image: Rudolph, 
-                title: "Rudolph", 
+                image: Rudolph,
+                title: "Rudolph",
                 message: "Rudolph the red nosed reindeer...(psst, click on the image to sing along)",
-                href: "https://www.youtube.com/watch?v=fJQqOzkcHjg&ab_channel=ChristmasSongsandCarols-LovetoSing"
             });
         }
         // mid tier
-        else if(id >= 30 && id < 60){
+        else if (id >= 30 && id < 60) {
             this.setState({
-                image: Socks, 
-                title: "Regifted Socks", 
+                id: "socks", 
+                image: Socks,
+                title: "Regifted Socks",
                 message: "They didn't even try to hide it...",
                 href: ""
             });
         }
         // bottom tier
-        else if(id >= 60 && id < 100){
+        else if (id >= 60 && id < 100) {
             this.setState({
-                image: Coal, 
-                title: "Coal", 
+                image: Coal,
+                title: "Coal",
                 message: "A flammable black rock. Click on the image to educate yourself :)",
                 href: "https://en.wikipedia.org/wiki/Coal"
             });
+        }
+    }
+
+    renderImage(){
+        if(this.state.title === "Rudolph"){
+            console.log();
         }
     }
 
