c29b6fcc9a0f018dba3a0269507a2d69b984e0b4
Kathy Kang <kck3due@virginia.edu>
2020-12-25 13:33:23 -0500

    adding css

--- client/src/components/Present.js
+++ client/src/components/Present.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Coal from '../images/coal.png';
+
+import './CSS/Present.css';
 
 class Present extends React.Component {
     render() {
