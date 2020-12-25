f05c0aa32dfc26b9162e75e997ce46f64880ac22
Kathy Kang <kck3due@virginia.edu>
2020-12-25 14:43:49 -0500

    load changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,13 +8,23 @@ import Load from './Load';
 import Present from './Present';
 
 class App extends React.Component {
+    state = { redirect: false, id: '' };
+
+    //randomly selects which present to display to the user
+    selectPresent() {
+        //selects a number between 0 - 99 inclusive
+        const maxNum = 100;
+        const selectedId = Math.floor(Math.random() * Math.floor(maxNum));
+        this.setState({ id: selectedId });
+    }
+
     render() {
         return (
             <div>
                 <Router history={history}>
                     <Header />
                     <Route path="/" exact component={OpenPresent} />
-                    <Route path="/load" exact component={Load} />
+                    <Route path="/load" exact component={Load}/>
                     <Route path="/present/:id" exact component={Present} />
                 </Router>
             </div>
